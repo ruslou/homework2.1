99e036a81c2976b449ba6db1aecf645e1334a40f
ruslou <165936535+ruslou@users.noreply.github.com>
2024-04-18 17:42:30 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,37 @@
+x = 38
+
+print('саламалекум!')
+if x < 0:
+    print('меньше нуля')
+print('саулбрат')
+
+a, b = 10, 5
+if a < b:
+    print('a > b')
+
+if a > b and a > 0:
+    print('success')
+
+if (a > b) and (a > 0 or b < 1000):
+    print('success')
+
+if 5 < b and b < 10:
+    print('success')
+
+if '34' > '123':
+    print('success')
+
+if '123' > '12':
+    print('success')
+
+if [1, 2] > [1, 1]:
+    print('success')
+
+if '6' > 5:
+    print('success')
+
+if [5,6] > 5:
+    print('success')
+
+if '6' != 5:
+    print('success')
